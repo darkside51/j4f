eaabd6a25e45c54a6b413b9896a6456dad4b83a3
stormtrooper <dark.side.8951@gmail.com>
2023-05-17 15:06:31 +0300

    hasher changes

--- app_j4f/engine_j4f/Engine/Core/Hash.h
+++ app_j4f/engine_j4f/Engine/Core/Hash.h
@@ -1,23 +1,25 @@
 #pragma once
 
+#include <cstddef>
+#include <cstdint>
+#include <functional>
 #include <type_traits>
 
 namespace engine {
 	template <class T>
-	inline void hash_combine(std::size_t& s, const T& v) noexcept { // https://stackoverflow.com/questions/19195183/how-to-properly-hash-the-custom-struct
-		std::hash<T> h;
-		s ^= h(v) + 0x9e3779b9 + (s << 6) + (s >> 2);
+	inline void hash_combine(std::size_t& s, T&& v) noexcept {
+        // https://stackoverflow.com/questions/19195183/how-to-properly-hash-the-custom-struct
+		s ^= std::hash<std::decay_t<T>>()(v) + 0x9e3779b9 + (s << 6) + (s >> 2);
 	}
 
-	///////
 	template <typename T>
-	inline void hash_combo(size_t& hash, const T& v) noexcept {
-		hash_combine(hash, v);
+	inline void hash_combo(size_t& hash, T&& v) noexcept {
+		hash_combine(hash, std::forward<T>(v));
 	}
 
 	template <typename T, typename... Args>
-	inline void hash_combo(size_t& hash, const T& v, Args&&... args) noexcept {
-		hash_combine(hash, v);
+	inline void hash_combo(size_t& hash, T&& v, Args&&... args) noexcept {
+		hash_combine(hash, std::forward<T>(v));
 		hash_combo(hash, std::forward<Args>(args)...);
 	}
 
@@ -27,19 +29,17 @@ namespace engine {
 		hash_combo(hash, std::forward<Args>(args)...);
 		return hash;
 	}
-
-
+    
 	// composition hash example 
 	/*inline size_t hash() const {
-		const size_t r1 = engine::hash_combine(a, b, s);
-
-		size_t r2 = 0;
-		engine::hash_combine(result, a);
-		engine::hash_combine(result, b);
-		engine::hash_combine(result, s);
+	    // use
+		const size_t r = engine::hash_combine(a, b, s);
+        // or
+		size_t r = 0;
+		engine::hash_combine(r, a);
+		engine::hash_combine(r, b);
+		engine::hash_combine(r, s);
 	}*/
-
-	///////
 
 	template<typename T, typename = void>
 	struct CustomHasher : std::false_type {};
@@ -49,7 +49,7 @@ namespace engine {
 
 	template <typename T>
 	struct Hasher {
-		inline std::size_t operator()(const T& k) const {
+		inline std::size_t operator()(const T& k) const noexcept {
 			if constexpr (CustomHasher<T>::value) {
 				return k.hash();
 			} else {
@@ -68,49 +68,49 @@ namespace engine {
 
 	template<>
 	struct Hasher<int8_t> {
-		inline std::size_t operator()(const int8_t& k) const { return k; }
-		inline std::size_t operator()(const int8_t&& k) const { return k; }
+		inline std::size_t operator()(const int8_t& k) const noexcept { return k; }
+		inline std::size_t operator()(const int8_t&& k) const noexcept { return k; }
 	};
 
 	template<>
 	struct Hasher<int16_t> {
-		inline std::size_t operator()(const int16_t& k) const { return k; }
-		inline std::size_t operator()(const int16_t&& k) const { return k; }
+		inline std::size_t operator()(const int16_t& k) const noexcept { return k; }
+		inline std::size_t operator()(const int16_t&& k) const noexcept { return k; }
 	};
 
 	template<>
 	struct Hasher<int32_t> {
-		inline std::size_t operator()(const int32_t& k) const { return k; }
-		inline std::size_t operator()(const int32_t&& k) const { return k; }
+		inline std::size_t operator()(const int32_t& k) const noexcept { return k; }
+		inline std::size_t operator()(const int32_t&& k) const noexcept { return k; }
 	};
 
 	template<>
 	struct Hasher<int64_t> {
-		inline std::size_t operator()(const int64_t& k) const { return k; }
-		inline std::size_t operator()(const int64_t&& k) const { return k; }
+		inline std::size_t operator()(const int64_t& k) const noexcept { return k; }
+		inline std::size_t operator()(const int64_t&& k) const noexcept { return k; }
 	};
 
 	template<>
 	struct Hasher<uint8_t> {
-		inline std::size_t operator()(const uint8_t& k) const { return k; }
-		inline std::size_t operator()(const uint8_t&& k) const { return k; }
+		inline std::size_t operator()(const uint8_t& k) const noexcept { return k; }
+		inline std::size_t operator()(const uint8_t&& k) const noexcept { return k; }
 	};
 
 	template<>
 	struct Hasher<uint16_t> {
-		inline std::size_t operator()(const uint16_t& k) const { return k; }
-		inline std::size_t operator()(const uint16_t&& k) const { return k; }
+		inline std::size_t operator()(const uint16_t& k) const noexcept { return k; }
+		inline std::size_t operator()(const uint16_t&& k) const noexcept { return k; }
 	};
 
 	template<>
 	struct Hasher<uint32_t> {
-		inline std::size_t operator()(const uint32_t& k) const { return k; }
-		inline std::size_t operator()(const uint32_t&& k) const { return k; }
+		inline std::size_t operator()(const uint32_t& k) const noexcept { return k; }
+		inline std::size_t operator()(const uint32_t&& k) const noexcept { return k; }
 	};
 
 	template<>
 	struct Hasher<uint64_t> {
-		inline std::size_t operator()(const uint64_t& k) const { return k; }
-		inline std::size_t operator()(const uint64_t&& k) const { return k; }
+		inline std::size_t operator()(const uint64_t& k) const noexcept { return k; }
+		inline std::size_t operator()(const uint64_t&& k) const noexcept { return k; }
 	};
 }
