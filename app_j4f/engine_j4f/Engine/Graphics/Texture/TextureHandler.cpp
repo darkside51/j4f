52e0130ce52729f2a92798b352187b132a03db6e
dark_side <dark.side.8951@gmail.com>
2023-05-23 22:43:55 +0300

    win64 spirv libs folders change

--- app_j4f/engine_j4f/Engine/Graphics/Texture/TextureHandler.cpp
+++ app_j4f/engine_j4f/Engine/Graphics/Texture/TextureHandler.cpp
@@ -11,8 +11,8 @@ namespace engine {
     }
 
     uint32_t TextureHandler::_decrease_counter() noexcept {
-        auto const count = m_counter.fetch_sub(1, std::memory_order_release) - 1;
-        if (count == 1) {
+        auto const count = m_counter.fetch_sub(1u, std::memory_order_release) - 1u;
+        if (count == 1u) {
             // todo: add cache and remove texture from cache and remove markToDelete from here
             Engine::getInstance().getModule<Graphics>()->getRenderer()->markToDelete(std::move(_texture));
         }
--- app_j4f/engine_j4f/Engine/Graphics/Texture/TextureHandler.h
+++ app_j4f/engine_j4f/Engine/Graphics/Texture/TextureHandler.h
@@ -11,21 +11,22 @@ namespace engine {
     class TextureHandler {
         using type = TextureHandler;
         using counter_type = std::atomic_uint32_t;
+        using texture_type = vulkan::VulkanTexture;
     public:
         template <typename ...Args>
         TextureHandler(Args&&...args) : _texture(std::forward<Args>(args)...) {}
         ~TextureHandler();
 
         uint32_t _decrease_counter() noexcept;
-        inline uint32_t _increase_counter() noexcept { return m_counter.fetch_add(1, std::memory_order_release) + 1; }
+        inline uint32_t _increase_counter() noexcept { return m_counter.fetch_add(1u, std::memory_order_release) + 1u; }
         [[nodiscard]] inline uint32_t _use_count() const noexcept { return m_counter.load(std::memory_order_relaxed); }
 
-        vulkan::VulkanTexture* texture() noexcept { return &_texture; }
-        const vulkan::VulkanTexture* texture() const noexcept { return &_texture; }
+        texture_type* texture() noexcept { return &_texture; }
+        const texture_type* texture() const noexcept { return &_texture; }
 
     private:
-        vulkan::VulkanTexture _texture;
-        std::atomic_uint32_t m_counter = 0;
+        texture_type _texture;
+        std::atomic_uint32_t m_counter = 0u;
     };
 
     using TexturePtr = linked_ptr<TextureHandler>;
