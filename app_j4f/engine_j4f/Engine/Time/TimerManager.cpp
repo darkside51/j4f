6ba685b7d0558203419452377cd7560d4dfac239
stormtrooper <dark.side.8951@gmail.com>
2023-07-26 23:29:37 +0300

    fix timerManager update

--- app_j4f/engine_j4f/Engine/Time/TimerManager.cpp
+++ app_j4f/engine_j4f/Engine/Time/TimerManager.cpp
@@ -6,25 +6,11 @@ namespace engine {
 
     void TimerManager::update() {
         auto const currentTime = steadyTime<std::chrono::milliseconds>();
-
-        uint32_t i = 0u;
-        const uint32_t sz = _timers.size();
-        std::vector<Timer *> _toRemove;
-        _toRemove.reserve(sz);
-        for (auto &&timer: _timers) {
-            if (timer.fireTime() <= currentTime) {
-                if (timer(currentTime) == TimerState::Finish) {
-                    _toRemove.push_back(&timer);
-                }
-            }
-            ++i;
-        }
-
-        if (_toRemove.empty()) return;
-
         _timers.erase(std::remove_if(_timers.begin(), _timers.end(),
-                                     [&_toRemove](const Timer &t) -> bool {
-                                         return std::find(_toRemove.begin(), _toRemove.end(), &t) != _toRemove.end();
+                                     [currentTime](Timer &timer) -> bool {
+                                         return
+                                            (timer.fireTime() <= currentTime &&
+                                            timer(currentTime) == TimerState::Finish);
                                      }),
                       _timers.end());
     }
