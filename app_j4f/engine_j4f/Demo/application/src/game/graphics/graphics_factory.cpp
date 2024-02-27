508203b39005722729c95fa204d0b6fce9d03788
stormtrooper <dark.side.8951@gmail.com>
2024-02-27 10:59:29 +0300

    gf++

--- app_j4f/engine_j4f/Demo/application/src/game/graphics/graphics_factory.cpp
+++ app_j4f/engine_j4f/Demo/application/src/game/graphics/graphics_factory.cpp
@@ -29,15 +29,87 @@ namespace game {
 
     void GraphicsFactory::loadObjects(const engine::Json & json) {
         // load gpu programs
-        for (auto & js: json["gpu_programs"]) {
-            auto name = js.value("name", "");
-            GPUProgramDescription description;
-            for (auto & sh : js["programs"]) {
-                auto type = sh.value("type", 0);
-                auto path = sh.value("path", "");
-                description.modules[type] = path;
+        if (auto array = json.find("gpu_programs"); array != json.end()) {
+            for (auto &js: *array) {
+                GPUProgramDescription description;
+                description.name = js.value("name", "");
+                for (auto &sh: js["programs"]) {
+                    auto const type = sh.value("type", 0);
+                    auto const path = sh.value("path", "");
+                    description.modules[type] = path;
+                }
+                _gpuPrograms.emplace_back(std::move(description));
             }
-            _gpuPrograms.emplace(name, description);
+        }
+
+        // load animations
+        if (auto array = json.find("animations"); array != json.end()) {
+            for (auto &js: *array) {
+                auto const name = js.value("name", "");
+                auto const path = js.value("path", "");
+                auto const speed = js.value("speed", 1.0f);
+                auto const infinity = js.value("infinity", true);
+
+                _animations.emplace_back(AnimationDescription{name, path, speed, infinity});
+            }
+        }
+
+        // meshes
+        if (auto array = json.find("meshes"); array != json.end()) {
+            _meshes.reserve(array->size());
+            for (auto &js: *array) {
+                auto const id = static_cast<uint16_t>(js.value("id", 0));
+                auto const path = js.value("path", "");
+                auto const latency = static_cast<uint8_t>(js.value("latency", 1));
+                _meshes.emplace_back(id, std::string(path), latency);
+            }
+        }
+
+        // graphics
+        if (auto array = json.find("graphics"); array != json.end()) {
+            for (auto &js: *array) {
+                auto const name = js.value("name", "");
+                auto const type = static_cast<uint8_t>(js.value("type", 0));
+                auto const gpuProgramName = js.value("gpu_program", "");
+
+                GraphicsDescription graphics;
+                graphics.type = static_cast<GraphicsType>(type);
+                graphics.descriptor = static_cast<uint8_t>(js.value("descriptor", 0));
+
+                auto it = std::find_if(_gpuPrograms.begin(), _gpuPrograms.end(),
+                             [&gpuProgramName](auto const & p){
+                                        return p.name == gpuProgramName;
+                });
+
+                if (it != _gpuPrograms.end()) {
+                    graphics.gpuProgramId = std::distance(_gpuPrograms.begin(), it);
+                }
+
+                if (auto animations = js.find("animations"); animations != js.end()) {
+                    graphics.animations.reserve(animations->size());
+
+                    auto getAnimId = [this](std::string_view name) -> size_t{
+                        auto it = std::find_if(_animations.begin(), _animations.end(),
+                                                 [name](auto const & anim){
+                                                     return anim.name == name;
+                                                 });
+                        if (it != _animations.end()) {
+                            return std::distance(_animations.begin(), it);
+                        }
+
+                        return 0u;
+                    };
+
+                    for (auto &animJs: *animations) {
+                        auto const name = animJs.value("name", "");
+                        auto const weight = animJs.value("weight", 0.0f);
+                        auto const id = static_cast<uint8_t>(animJs.value("id", 0));
+
+                        graphics.animations.emplace_back(getAnimId(name), weight, id);
+                    }
+                }
+                _descriptions.emplace(name, std::move(graphics));
+            }
         }
     }
 }
--- app_j4f/engine_j4f/Demo/application/src/game/graphics/graphics_factory.h
+++ app_j4f/engine_j4f/Demo/application/src/game/graphics/graphics_factory.h
@@ -9,6 +9,7 @@
 #include <cstdint>
 #include <string>
 #include <string_view>
+#include <tuple>
 #include <variant>
 #include <vector>
 #include <unordered_map>
@@ -20,38 +21,41 @@ namespace game {
         enum class Module : uint8_t {
             Vertex = 0u, Fragment, Geometry, Compute, Count
         };
-
+        std::string name;
         std::array<std::string, static_cast<size_t>(Module::Count)> modules;
     };
 
     struct AnimationDescription {
+        std::string name;
         std::string path;
-        uint8_t id = 0u;
         float speed = 1.0f;
         bool infinity = true;
     };
 
+    enum class GraphicsType : uint8_t {
+        None = 0u, Reference = 1u, Mesh, Particles, Plane,
+    };
+
     struct MeshDescription {
+        uint16_t id = 0u;
         std::string path;
         uint8_t latency = 1u;
+
+        MeshDescription(uint16_t id, std::string&& path, uint8_t latency) :
+            id(id), path(std::move(path)), latency(latency) {}
     };
 
     struct GraphicsDescription {
-        enum class Type : uint8_t {
-            None = 0u, Reference = 1u, Mesh, Particles, Plane,
-        };
-
-        Type type = Type::None;
-        uint16_t order = 0u;
-
-        engine::ref_ptr<GPUProgramDescription> gpuProgram;
-        std::vector<std::pair<engine::ref_ptr<AnimationDescription>, float>> animations; // animation + weight
-        std::variant<std::monostate, MeshDescription> typeDescription;
+        GraphicsType type = GraphicsType::None;
+        size_t gpuProgramId = 0u;
+        std::vector<std::tuple<size_t, float, uint8_t>> animations; // animationid + weight + id
+        uint16_t descriptor = 0u;
         //...
     };
 
     struct ObjectDescription {
         engine::ref_ptr<GraphicsDescription> description;
+        uint16_t order = 0u;
         std::vector<ObjectDescription> children;
     };
 
@@ -63,7 +67,9 @@ namespace game {
     private:
         std::unordered_map<std::string, GraphicsDescription> _descriptions;
         std::unordered_map<std::string, ObjectDescription> _objects;
-        std::unordered_map<std::string, GPUProgramDescription> _gpuPrograms;
-        std::unordered_map<std::string, AnimationDescription> _animations;
+
+        std::vector<GPUProgramDescription> _gpuPrograms;
+        std::vector<AnimationDescription> _animations;
+        std::vector<MeshDescription> _meshes;
     };
 }
