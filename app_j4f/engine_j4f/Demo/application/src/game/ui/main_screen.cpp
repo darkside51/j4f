64e0995f729fbbdda5ad9301280419391d0bf205
dark_side <dark.side.8951@gmail.com>
2024-03-13 23:44:02 +0300

    demo mainscreen minimap

--- app_j4f/engine_j4f/Demo/application/src/game/ui/main_screen.cpp
+++ app_j4f/engine_j4f/Demo/application/src/game/ui/main_screen.cpp
@@ -9,6 +9,9 @@
 
 namespace game {
 
+	constexpr float kWindowRound = 20.0f;
+	constexpr float kFrameRound = 10.0f;
+
 	class ImGuiStyleColorChanger {
 	public:
 		template<typename... Args>
@@ -21,20 +24,16 @@ namespace game {
 		}
 	};
 
-	void MainScreen::draw(const float delta) {
-		//ImGui::ShowDemoWindow();
-
+	float MainScreen::drawBottomPanel() {
 		ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize
 			| ImGuiWindowFlags_NoSavedSettings
 			| ImGuiWindowFlags_NoFocusOnAppearing
 			| ImGuiWindowFlags_NoNav
 			| ImGuiWindowFlags_NoMove;
 
-		constexpr float kWindowRound = 20.0f;
-		constexpr float kFrameRound = 10.0f;
 		const float height = 70.0f;
 		const float btnSize = height - 10.0f;
-		const uint8_t btnCount = 10u;
+		const uint8_t btnCount = 6u;
 		const float btnSpace = 5.0f;
 		const float width = (btnSize + btnSpace) * btnCount - btnSpace + 2 * (kWindowRound - kFrameRound);
 		const float hspace = 10.0f;
@@ -43,7 +42,7 @@ namespace game {
 		const ImVec2 work_size = viewport->WorkSize;
 
 		ImVec2 window_pos;
-		window_pos.x =  (work_size.x - width) * 0.5f;
+		window_pos.x = (work_size.x - width) * 0.5f;
 		window_pos.y = work_size.y - (height + hspace);
 		ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always);
 		ImGui::SetNextWindowSize(ImVec2(width, height), ImGuiCond_Always);
@@ -54,13 +53,77 @@ namespace game {
 		std::swap(style.WindowRounding, windowRounding);
 		std::swap(style.FrameRounding, frameRounding);
 
-		const auto bgColor = IM_COL32(0, 0, 0, 100);
+		if (ImGui::Begin("##skills_panel", nullptr, window_flags)) {
 
+			for (uint8_t i = 0u; i < btnCount; ++i) {
+				ImGui::SetCursorPos({ (kWindowRound - kFrameRound) + (btnSize + btnSpace) * i, 5.0f });
+				if (ImGui::Button(engine::fmt_string("#act_{}", i), { btnSize, btnSize })) {
+					engine::Engine::getInstance().getModule<engine::Bus>().sendEvent(i);
+				}
+			}
+
+			ImGui::End();
+		}
+
+		std::swap(style.WindowRounding, windowRounding);
+		std::swap(style.FrameRounding, frameRounding);
+
+		return width;
+	}
+
+	void MainScreen::drawMapPanel(const float x, float delta) {
+		ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize
+			| ImGuiWindowFlags_NoSavedSettings
+			| ImGuiWindowFlags_NoFocusOnAppearing
+			| ImGuiWindowFlags_NoNav
+			| ImGuiWindowFlags_NoMove;
+		const ImGuiViewport* viewport = ImGui::GetMainViewport();
+		const ImVec2 work_size = viewport->WorkSize;
+
+		constexpr float kLocatorSpeed = 90.0f;
+		const float width = 150.0f;
+		const float height = _showMap ? 150.0f : 10.0f;
+		const float wspace = 160.0f;
+		const float hspace = 10.0f;
+
+		ImVec2 window_pos;
+		window_pos.x = std::max(x - wspace - width, 0.0f);
+		window_pos.y = work_size.y - (height + hspace);
+		ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always);
+		ImGui::SetNextWindowSize(ImVec2(width, height), ImGuiCond_Always);
+
+		ImGuiStyleColorChanger bg{ ImGuiCol_WindowBg, IM_COL32(100, 100, 100, 200) };
+
+		if (ImGui::Begin("##map_panel", nullptr, window_flags)) {
+			ImGui::SetCursorPos({0.0f, 0.0f});
+			if (ImGui::Button("##minimap", { width, 10.0f })) {
+				_showMap = !_showMap;
+			}
+			if (_showMap) {
+				auto&& drawList = ImGui::GetWindowDrawList();
+				drawList->AddRectFilled({ window_pos.x + width * 0.5f - 0.5f, window_pos.y }, { window_pos.x + width * 0.5f + 0.5f, window_pos.y + height }, 0x33'ff'ff'ffu);
+				drawList->AddRectFilled({ window_pos.x, window_pos.y + height * 0.5f - 0.5f }, { window_pos.x + width, window_pos.y + height * 0.5f + 0.5f }, 0x33'ff'ff'ffu);
+				drawList->AddCircleFilled({ window_pos.x + width * 0.5f, window_pos.y + height * 0.5f }, 3.0f, 0xff'77'77'ffu, 16);
+
+				_locatorSearchRadius += delta * kLocatorSpeed;
+				drawList->AddCircle({ window_pos.x + width * 0.5f, window_pos.y + height * 0.5f }, _locatorSearchRadius, 0xff'77'77'77u, 32);
+				if (_locatorSearchRadius >= width * 0.5f) {
+					_locatorSearchRadius = 0.0f;
+				}
+			}
+			ImGui::End();
+		}
+	}
+
+	void MainScreen::draw(const float delta) {
+		//ImGui::ShowDemoWindow();
+
+		const auto bgColor = IM_COL32(0, 0, 0, 100);
 		const std::array<ImGuiStyleColorChanger, 14u> changedColors = {
 				ImGuiStyleColorChanger{ImGuiCol_Text, IM_COL32(200, 200, 200, 200)},
-				{ImGuiCol_Button, IM_COL32(50, 50, 50, 200)},
-				{ImGuiCol_ButtonActive, IM_COL32(75, 75, 75, 200)},
-				{ImGuiCol_ButtonHovered, IM_COL32(0, 0, 0, 200)},
+				{ImGuiCol_Button, IM_COL32(20, 20, 20, 100)},
+				{ImGuiCol_ButtonActive, IM_COL32(0, 0, 0, 50)},
+				{ImGuiCol_ButtonHovered, IM_COL32(0, 0, 0, 100)},
 				{ImGuiCol_WindowBg, bgColor},
 				{ImGuiCol_TitleBg, bgColor},
 				{ImGuiCol_TitleBgCollapsed, bgColor},
@@ -73,20 +136,11 @@ namespace game {
 				{ImGuiCol_PlotHistogramHovered, IM_COL32(0, 0, 0, 200)},
 		};
 
-		if (ImGui::Begin("##skills_panel", nullptr, window_flags)) {
-			
-			for (uint8_t i = 0u; i < btnCount; ++i) {
-				ImGui::SetCursorPos({ (kWindowRound - kFrameRound) + (btnSize + btnSpace) * i, 5.0f });
-				if (ImGui::Button(engine::fmt_string("#act_{}", i), { btnSize, btnSize })) {
-                    engine::Engine::getInstance().getModule<engine::Bus>().sendEvent(i);
-				}
-			}
+		const ImGuiViewport* viewport = ImGui::GetMainViewport();
+		const ImVec2 work_size = viewport->WorkSize;
 
-			ImGui::End();
-		}
-
-		std::swap(style.WindowRounding, windowRounding);
-		std::swap(style.FrameRounding, frameRounding);
+		auto const w = drawBottomPanel();
+		drawMapPanel((work_size.x - w) * 0.5f, delta);
 	}
 
 }
--- app_j4f/engine_j4f/Demo/application/src/game/ui/main_screen.h
+++ app_j4f/engine_j4f/Demo/application/src/game/ui/main_screen.h
@@ -1,11 +1,18 @@
 #pragma once
 
 #include "../graphics/ui_manager.h"
+#include <cstdint>
 
 namespace game {
 
 	class MainScreen : public UIModule {
 		void draw(const float delta) override;
+		float drawBottomPanel();
+		void drawMapPanel(const float x, const float delta);
+
+	private:
+		bool _showMap = false;
+		float _locatorSearchRadius = 0.0f;
 	};
 
 }
